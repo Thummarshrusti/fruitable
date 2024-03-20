d2ccf729a587c5bb404f532122cc9b3a5221e51c
Thummarshrusti <shrustithummar2002@gmail.com>
2024-03-20 10:14:20 +0530

    product-table

--- src/Routes/AdminRoutes.js
+++ src/Routes/AdminRoutes.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Products from '../admin/container/Products/Products';
+import Layout from '../admin/components/Layout/Layout';
+import Review from '../admin/container/Review/Review';
 
 function AdminRoutes(props) {
     return (
-       <Routes>
-        <Route exact path='/products' element={<Products />} />
-       </Routes>
+        <Layout>
+            <Routes>
+                <Route exact path='/products' element={<Products />} />
+                <Route exact path='/Review' element={<Review />}/>
+            </Routes>
+        </Layout>
+
     );
 }
 
